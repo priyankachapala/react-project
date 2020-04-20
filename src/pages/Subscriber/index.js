e92968e836742f92018b80d34e07529b5ab4b228
Aakanksha Duggal <ad694@snu.edu.in>
2020-04-20 12:17:52 -0400

    Update index.js

--- src/pages/Subscriber/index.js
+++ src/pages/Subscriber/index.js
@@ -5,5 +5,26 @@ export default function subDashboard() {
     <h1> Hello, I am dashboard for subscriber </h1>
     <br/>
     <a href="http://localhost:3002/"> back</a>
+  
+    <h2>Select destination</h2>
+    <p>Select your route, choose the start point and destination.</p>
+
+    <div class="dropdown" style="float:left;">
+      <button class="dropbtn">StartPoint</button>
+      <div class="dropdown-content" style="left:0;">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+    </div>
+
+    <div class="dropdown" style="float:right;">
+      <button class="dropbtn">Destination</button>
+      <div class="dropdown-content">
+        <a href="#">Link 1</a>
+        <a href="#">Link 2</a>
+        <a href="#">Link 3</a>
+      </div>
+      </div>
   </div>
 }
